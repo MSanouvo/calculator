d2cb97e8ba674b3105007d761efbc7d3d407702b
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-10-23 15:05:10 -0700

    Added functions to js file
    - js file previously empty
    - added basic functions that would be expected from a calculator
    - need to add event listeners to functions

---
+++ script.js
@@ -0,0 +1,35 @@
+//functions
+function addition(a,b){
+    return a+b;
+};
+
+function subtraction(a,b){
+    return a-b;
+};
+
+function multiplication(a,b){
+    return a*b;
+};
+
+function division(a,b){
+    if(b ===0){
+        return "Error: Can't divide by 0";
+    }else{
+        return a/b;
+    }
+}
+
+function remainder(a,b){
+    return a%b;
+}
+
+function factorial(a,b){
+    return a**b;
+}
+
+function calculate(operator, a, b){
+    return operator(a,b);
+}
+
+console.log(calculate(factorial, 2, 3))
+
