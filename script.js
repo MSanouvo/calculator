d3fe9f63dbb3a38c60eae1f3801929d1e343639a
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-10-23 22:15:01 -0700

    added some functionality to operators and clear
    - display screen now works properly showing numbers
    - operators now set a second variable for functions to work
    - clear now deletes display variables
    - abundance of if logic, needs refactoring

--- script.js
+++ script.js
@@ -44,15 +44,47 @@ display.textContent = displayText1 //display empty container at program start
 //event delegation for buttons in container
 buttonContainer.addEventListener('click', (e) =>{
     let target = e.target;
-    console.log(target.id)
-    //button press events for numbers
-    if(target.id < 10){
-        displayText1 += target.id
-        display.textContent = displayText1
-    }else if(target.id === 'zero'){
-        if(displayText1 != ''){
-            displayText1 += '0'
+    
+    //condition for display 1
+    if(operator != null){
+        //button press events for numbers
+        if(target.id < 10){
+            displayText1 += target.id
             display.textContent = displayText1
-        } //Doesn't add 0 to an empty container
+        }else if(target.id === 'zero'){
+            if(displayText1 != ''){
+                displayText1 += '0'
+                display.textContent = displayText1
+            } //Doesn't add 0 to an empty container
+        }
+
+        if(target.id === 'clear'){
+            displayText1 = ''
+            display.textContent = displayText1
+        }
+    }else{//condition for display 2
+        if(target.id < 10){
+            displayText2 += target.id
+            display.textContent = displayText2
+        }else if(target.id === 'zero'){
+            if(displayText2 != ''){
+                displayText2 += '0'
+                display.textContent = displayText2
+            } //Doesn't add 0 to an empty container
+        }
+        //clear 
+        if(target.id === 'clear'){
+            displayText2 = ''
+            display.textContent = displayText2
+        }
     }
+    
+    
+    //button press events for operators
+    if(target.id === '+' || target.id ===  '-' || target.id ===  'x' || target.id ===  '/'){
+        operator = target.id
+        console.log(operator) 
+    }
+
+    
 })
